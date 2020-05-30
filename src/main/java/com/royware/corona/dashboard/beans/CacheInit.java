84128ed8487a1b9f273208d5f2e0285058a4dd86
Roy Chancellor <roychance600@gmail.com>
2020-05-29 19:33:34 -0700

    Fixed log message for environment so it reads properly in AWS
    * Use Spring Environment class and getProperty method to get Spring's
    value of the environment.

--- src/main/java/com/royware/corona/dashboard/beans/CacheInit.java
+++ src/main/java/com/royware/corona/dashboard/beans/CacheInit.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import com.royware.corona.dashboard.enums.data.CacheKeys;
@@ -16,11 +17,15 @@ public class CacheInit {
 	@Autowired
 	@Qualifier("world")
 	private ExternalDataService worldDataService;
+	
+	@Autowired
+	private Environment env;
+	
 	private static final Logger log = LoggerFactory.getLogger(CacheInit.class);
 	
 	@EventListener(ContextRefreshedEvent.class)
 	public void contextRefreshedEvent() {
-		log.info("Application has successfully started in environment: {}", System.getenv("ENVIRONMENT"));
+		log.info("Application has successfully started in environment: {}", env.getProperty("ENVIRONMENT"));
 		log.info("In CacheInit class: worldDataService hashcode: " + this.hashCode());
 		worldDataService.makeDataListFromExternalSource(CacheKeys.CACHE_KEY_WORLD.getName());
 	}
