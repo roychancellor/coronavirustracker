a22ba50489e62db258392b44b06f3704c68898ec
Roy Chancellor <roychance600@gmail.com>
2020-08-22 14:07:58 -0700

    Updated CSV header names to reflect current dashboard statistics

--- src/main/java/com/royware/corona/dashboard/enums/charts/ChartCsvHeaders.java
+++ src/main/java/com/royware/corona/dashboard/enums/charts/ChartCsvHeaders.java
@@ -1,31 +1,19 @@
 package com.royware.corona.dashboard.enums.charts;
 
 public enum ChartCsvHeaders {
-	CASES_TIME_SERIES("totalCases"),
-	CASES_RATE("rateOfCases%"),
-	CASES_ACCEL("accelCases%"),
+	CASES_TIME_SERIES("totalPositives"),
+	CASES_RATE("dailyRateOfPositives%"),
+	CASES_ACCEL("dailyAccelCases%"),
 	CASES_CHG_BY_CASES("chgCasesOverCases"),
-	CASES_TOTAL_CURRENT("totalCurrentCases"),
+	CASES_TOTAL_CURRENT("positivityPer100K"),
 	DEATHS_TIME_SERIES("totalDeaths"),
-	DEATHS_RATE("rateOfDeaths%"),
-	DEATHS_ACCEL("accelDeaths%"),
+	DEATHS_RATE("dailyRateOfDeaths%"),
+	DEATHS_ACCEL("dailyAccelDeaths%"),
 	DEATHS_CHG_BY_CASES("chgDeathsOverDeaths"),
-	TESTS_TIME_SERIES("totalTests"),
-	TESTS_RATIO("ratioPositivesToTests"),
+	TESTS_TIME_SERIES("totalTestsConducted"),
+	TESTS_RATIO("ratioDailyPositivesToDailyTests"),
 	HOSP_CURRENT("hospCurrent"),
 	HOSP_CUMULATIVE("hospCumulative");
-//	CASES_TIME_SERIES("totalCases,dailyCases"),
-//	CASES_RATE("rateOfCases,4dayMovAvgRateCases"),
-//	CASES_ACCEL("accelCases,4DayMovAvgAccelCases"),
-//	CASES_CHG_BY_CASES("chgCasesOverCases,expLine"),
-//	DEATHS_TIME_SERIES("totalDeaths,dailyDeaths"),
-//	DEATHS_RATE("rateOfDeaths,4dayMovAvgRateDeaths"),
-//	DEATHS_ACCEL("accelDeaths,4dayMovAvgAccelDeaths"),
-//	DEATHS_CHG_BY_CASES("chgDeathsOverDeaths,expLine"),
-//	TESTS_TIME_SERIES("totalTests,4dayMovAvgNewTests"),
-//	TESTS_RATIO("ratioPositivesToTests,4dayMovAvgTestRatio"),
-//	HOSP_CURRENT("hospCurrent,4dayMovAvgNewHosp"),
-//	HOSP_CUMULATIVE("hospCumulative,4dayMovAvgNewHosp");
 	
 	public final String headerName;
 	
