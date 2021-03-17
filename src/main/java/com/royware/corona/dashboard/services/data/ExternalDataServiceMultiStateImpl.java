cc9219194f7eef168ea37a78e599c29df057bd46
Roy Chancellor <roychance600@gmail.com>
2021-03-16 20:25:57 -0700

    Fixed multi-region dashboard maker
    * Had to rewire the ExternalDataServiceMultiStateImpl method
    makeDataListFromExternalSource to call the
    singleStateDataService.makeDataListFromExternalSource for each state
    (actually, was duplicated code, so this is more DRY anyway).

--- src/main/java/com/royware/corona/dashboard/services/data/ExternalDataServiceMultiStateImpl.java
+++ src/main/java/com/royware/corona/dashboard/services/data/ExternalDataServiceMultiStateImpl.java
@@ -1,18 +1,13 @@
 package com.royware.corona.dashboard.services.data;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
-import com.royware.corona.dashboard.enums.data.DataUrls;
 import com.royware.corona.dashboard.interfaces.data.ExternalDataService;
 import com.royware.corona.dashboard.model.data.UnitedStatesData;
 
@@ -21,22 +16,18 @@ import com.royware.corona.dashboard.model.data.UnitedStatesData;
  */
 @Component("multiState")
 public class ExternalDataServiceMultiStateImpl implements ExternalDataService {
-	@Autowired
-	private RestTemplate restTemplate;
-	
 	private static final Logger log = LoggerFactory.getLogger(ExternalDataServiceMultiStateImpl.class);
+	
+	@Autowired
+	@Qualifier(value = "singleStateCDCSource")
+	private ExternalDataService singleStateDataService;
 	
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<UnitedStatesData> makeDataListFromExternalSource(String multiRegionState) {
-		String url = DataUrls.STATE_DATA_URL_START.getName() + multiRegionState.toLowerCase() + DataUrls.STATE_DATA_URL_END.getName();
-		log.info("***** MULTI_REGION: ABOUT TO HIT ENDPOINT FOR STATE DATA AT " + url + " FOR " + multiRegionState);
-		UnitedStatesData[] stateDataArray = restTemplate.getForObject(url, UnitedStatesData[].class);
-		List<UnitedStatesData> stateDataList = new ArrayList<>(Arrays.asList(stateDataArray));
-		Collections.reverse(stateDataList);
-		stateDataList.removeIf(unitedStatesCase -> (unitedStatesCase.getDateInteger() < US_CUTOFF_DATE));
-		
-		log.info("***** FINISHED GETTING STATE: " + multiRegionState + " ****");
+		log.info("***** MULTI_REGION: ABOUT TO HIT ENDPOINT FOR STATE DATA FOR: " + multiRegionState);
+		List<UnitedStatesData> stateDataList = singleStateDataService.makeDataListFromExternalSource(multiRegionState);
+		log.info("***** FINISHED GETTING DATA FOR STATE: " + multiRegionState + " ****");
 		
 		return stateDataList;
 	}
